c59af2a9b6ef94cbe85280444705f3094cb6e8b3
Sascha Curth <scurth@Saschas-MacBook-Pro.local>
2016-10-08 15:49:08 +0200

    dynamic path for reset

--- bin/cmd/reset_pre.sh
+++ bin/cmd/reset_pre.sh
@@ -2,5 +2,11 @@ CMD_DESCRIPTION="reset mysql server"
 
 athena.docker.add_option "-ti"
 
-athena.docker.mount_dir ~/sandbox/dbdata /var/lib/mysql
+athena.usage 1 "<dbdata directory>"
+
+# arguments are found below
+dbdata="$(athena.path 1)"
+
+
+athena.docker.mount_dir "${dbdata}" /var/lib/mysql
 
